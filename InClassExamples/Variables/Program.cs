e6b95ac560c3264c33ab9f5d2469453d56c9ffb5
Riley Carr <rileycarr@ou.edu>
2020-01-22 12:34:12 -0600

    Created the in class examples solution and created a project for variables

---
+++ InClassExamples/Variables/Program.cs
@@ -0,0 +1,16 @@
+﻿//Riley Carr
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
