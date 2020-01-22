be1bac19c098bd77eb3ad1bf701396a5ed72e21b
Riley Carr <rileycarr@ou.edu>
2020-01-22 12:54:24 -0600

    Created variables for firstName, lastName and favoriteNumber. Also derived crazyNumber from it.

--- InClassExamples/Variables/Program.cs
+++ InClassExamples/Variables/Program.cs
@@ -11,6 +11,26 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Create an string named firstName
+            string firstName;
+
+            //Assign a value to firstName (use your name)
+            firstName = "Riley";
+
+            //Create an int named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 13;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "Carr";
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName + " has the favorite number of " + favoriteNumber
+                + " \nwhich equates to a crazy number of " + crazyNumber);
+
+            // MAKE IT DYNAMIC
+
+            Console.ReadKey();
         }
     }
 }
