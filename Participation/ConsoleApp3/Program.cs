0f3599e0cf91bf7f7ade82f56dbcb730d0c9ff08
Riley Carr <rileycarr@ou.edu>
2020-03-09 13:14:53 -0500

    almost finished FunctionsCalculator

--- Participation/ConsoleApp3/Program.cs
+++ Participation/ConsoleApp3/Program.cs
@@ -19,47 +19,75 @@ namespace FunctionsCalculator
         static void Main(string[] args)
         {
             string answer2;
-            
-            
-            List<double> Values = new List<double>();
 
+            List<double> Values = new List<double>();
+                string answer;
+                double Value1;
+                double Value2;
+                double result;
             do
             {
                 Console.WriteLine("Please enter the type of calculation you want to perform");
-                string answer = Console.ReadLine();
-
+                answer = Console.ReadLine();
                 Console.WriteLine("Please enter one value");
-                double Value1 = Convert.ToDouble(Console.ReadLine());
-
+                Value1 = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine("Please enter another value");
-                double Value2 = Convert.ToDouble(Console.ReadLine());
+                Value2 = Convert.ToDouble(Console.ReadLine());
 
                 if (answer == "Add")
                 {
-                    double Addsum = Add(Value1, Value2);
-                    Console.WriteLine(Value1 + " + " + Value2 + " = " + Addsum);
+                    result = Add(Value1, Value2);
+                    Console.WriteLine(Value1 + " + " + Value2 + " = " + result);
                 }
                 else if (answer == "Subtract")
                 {
-                    double SubtractAnswer = Subtract(Value1, Value2);
-                    Console.WriteLine(Value1 + " - " + Value2 + " = " + SubtractAnswer);
+                    result = Subtract(Value1, Value2);
+                    Console.WriteLine(Value1 + " - " + Value2 + " = " + result);
                 }
-                else if (answer == "Multiply)
+                else if (answer == "Multiply")
                 {
-                    double MultiplyAnswer = Multiply(Value1, Value2);
-                    Console.WriteLine(Value1 + " x " + Value2 + " = " + MultiplyAnswer);
+                    result = Multiply(Value1, Value2);
+                    Console.WriteLine(Value1 + " x " + Value2 + " = " + result);
                 }
                 else if (answer == "Divide")
                 {
-                    double DivideAnswer = Divide(Value1, Value2);
-                    Console.WriteLine(Value1 + " /" + Value2 + " = " + DivideAnswer);
+                    result = Divide(Value1, Value2);
+                    Console.WriteLine(Value1 + " /" + Value2 + " = " + result);
                 }
 
                 Console.WriteLine("Would you like to perform a new calculation?");
                 answer2 = Console.ReadLine();
+                if (answer2 == "No")
+                {
+                    Console.WriteLine("Thank you for using this service");
+                }
+                else if (answer2 == "Yes")
+                {
+                    if (answer == "Add")
+                    {
+                        result = Add(Value1, Value2);
+                        Console.WriteLine(Value1 + " + " + Value2 + " = " + result);
+                    }
+                    else if (answer == "Subtract")
+                    {
+                        result = Subtract(Value1, Value2);
+                        Console.WriteLine(Value1 + " - " + Value2 + " = " + result);
+                    }
+                    else if (answer == "Multiply")
+                    {
+                        result = Multiply(Value1, Value2);
+                        Console.WriteLine(Value1 + " x " + Value2 + " = " + result);
+                    }
+                    else if (answer == "Divide")
+                    {
+                        result = Divide(Value1, Value2);
+                        Console.WriteLine(Value1 + " /" + Value2 + " = " + result);
+                    }
+                }
 
             } while (answer2.ToLower() != "no");
-         
+
+           
         }
 
         static double Add(double v1, double v2)
