742b649b7620643b39b70e1298232102e675e738
Riley Carr <rileycarr@ou.edu>
2020-04-16 19:04:39 -0500

    WPF participation

---
+++ Participation/WPF-1-Participation/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_1_Participation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string Name, FavColor;
+            Name = txtName.Text;
+            FavColor = txtColor.Text;
+
+            if (FavColor == "pink")
+            {
+                MessageBox.Show($"Wow {Name}, we have the same favorite color!");
+            }
+            else
+            {
+                MessageBox.Show($"{Name} that is really cool, I never would have expected your favorite color to be { FavColor}");
+            }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+    }
+
+      
+    }
+}
