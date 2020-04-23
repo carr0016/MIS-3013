03900e560076303ca8cdf5f959237022664a2d20
Riley Carr <rileycarr@ou.edu>
2020-04-23 12:07:01 -0500

    work on student

---
+++ Participation/WPF-Student/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Student
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string StudentID;
+            StudentID = txtStudentID.Text;
+            int StudentIDNum = Convert.ToInt32(StudentID);
+            string FirstName;
+            FirstName = txtFirstName1.Text;
+            string LastName;
+            LastName = txtLastName.Text;
+            string FavoriteColor;
+            FavoriteColor = txtFavoriteColor.Text;
+            
+
+        }
+    }
+}
---
+++ Participation/WPF-Student/StudentClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Student
+{
+    class StudentClass
+    {
+       
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set;  }
+        public string FavoriteColor { get; set; }
+        public Student ()
+        {
+
+        }
+    }
+}
