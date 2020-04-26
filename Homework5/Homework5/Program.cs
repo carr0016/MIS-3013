85d3fce77d313f37e495174282a5b303d636c8d1
Riley Carr <rileycarr@ou.edu>
2020-04-26 17:29:04 -0500

    homework 8 working on

--- Homework5/Homework5/Program.cs
+++ Homework5/Homework5/Program.cs
@@ -10,10 +10,30 @@ namespace Homework5
     {
         static void Main(string[] args)
         {
-            
-            Console.ReadKey();
+            Dictionary<string, int> Directory = new Dictionary<string, int>();
+            Console.WriteLine("Would you like to add a new category");
+            string answer = Console.ReadLine();
+           if (answer == "Yes")
+            {
+                Console.WriteLine("Enter the Name ");
+                string Name = Console.ReadLine();
+                Console.WriteLine("Enter the Age ");
+                int Age = Convert.ToInt32(Console.ReadLine());
+                Directory.Add(Name, Age);
+            }
+            Console.WriteLine("Press key to display the contents of your dictionary..");
+            Console.ReadLine();
+            foreach (var item in Directory)
+            {
+                Console.WriteLine("Name : " + item.Key);
+                Console.WriteLine("Age : " + item.Value);
+            }
+            Console.ReadLine();
 
+            Console.ReadKey();
         }
-        }
+
     }
+        }
+    
 
---
+++ Homework8-WPFGame/Homework8-WPFGame/MarkType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8_WPFGame
+{
+    public enum MarkType
+    {
+        Free,
+        Nought,
+        Cross,
+    }
+}
