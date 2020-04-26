997a20b3898b1feb37ab3a471bfcfe0c0f6839a6
Riley Carr <rileycarr@ou.edu>
2020-04-26 11:19:50 -0500

    arrays

--- Homework5/Arrays/Program.cs
+++ Homework5/Arrays/Program.cs
@@ -10,6 +10,27 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+
+            string[] FirstName = new string[1];
+            string[] LastName = new string[1];
+
+            for (int i = 0; i < CourseName.Length; i++)
+            {
+                Console.WriteLine("Please input a course name");
+                CourseName[i] = Console.ReadLine();
+                Console.WriteLine("Please input one course number");
+                CourseNumber[i] = Convert.ToDouble(Console.ReadLine());
+
+            }
+
+            for (int i = 0; i < CourseNumber.Length; i++)
+            {
+                Console.WriteLine(CourseName[i] + CourseNumber[i]);
+            }
+
+           
+
+            Console.ReadKey();
         }
     }
 }
--- Homework5/ConsoleApp1/Program.cs
+++ Homework5/ConsoleApp1/Program.cs
@@ -10,6 +10,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            List<double> GradesList = new List<double>();
+            string Answer;
+
+            do
+            {
+                Console.WriteLine("Enter your grade");
+                double Grade = Convert.ToDouble(Console.ReadLine());
+                GradesList.Add(Grade);
+
+                Console.WriteLine("Do you want to enter another exam grade");
+                Answer = Console.ReadLine();
+
+            } while (Answer == "yes");
+
+
+            double Sum = 0;
+
+            foreach (var Grade in GradesList)
+            {
+                Sum = Sum + GradesList[i];
+            }
+
         }
     }
 }
